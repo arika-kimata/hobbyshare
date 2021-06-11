f41723ddd49fd8f815f028ca5066cd96b426d31c
リリ <riri@ririnoMacBook-Pro.local>
2021-06-11 13:13:36 +0900

    ユーザー詳細画面の作成

--- db/migrate/20210610123216_devise_create_users.rb
+++ db/migrate/20210610123216_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.text   :profile,            null: false
+      t.text   :category,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
